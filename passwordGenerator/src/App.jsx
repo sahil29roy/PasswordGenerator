ce55403e9aadd5f5f22b1ddc00ae794b7345d9b3
sahil <sahil29roy@gmail.com>
2024-08-10 16:10:51 +0530

    change tailwind css

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -38,10 +38,10 @@ function App() {
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, Symbols, passwordGenerator])
-  
+
   return (
 
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-blue-500">
       <h1 className='text-white text-center my-3'>Password generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
@@ -54,7 +54,7 @@ function App() {
         />
         <button
           onClick={copyPasswordToClipboard}
-          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          className='outline-none bg-green-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
 
       </div>
